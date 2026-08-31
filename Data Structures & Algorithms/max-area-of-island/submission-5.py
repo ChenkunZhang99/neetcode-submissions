250ce894a06611464786a4ea6af97fff3bfd710e
ChenkunZhang99 <mm10237207@gmail.com>
2026-08-30 20:44:32 -0700

    Add: max-area-of-island - submission-5

---
+++ Data Structures & Algorithms/max-area-of-island/submission-5.py
@@ -0,0 +1,37 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        max_val = 0
+        for row in range (len(grid)):
+            for col in range (len(grid[0])):
+                if grid[row][col] == 1:
+                    max_val = max (max_val, self.maxArea( grid, row, col))
+        return max_val
+
+
+    # def maxArea(self, grid, row, col, max_val):
+    #     rows = len(grid)
+    #     cols = len(grid[0])
+    #     if row < 0 or row >= rows or col < 0 or col >= cols:
+    #         return max_val
+    #     if grid[row][col] == 0:
+    #         return max_val
+    #     grid[row][col] = 0
+    #     max_val += 1
+    #     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    #     for d_row, d_col in directions:
+    #         max_val = self.maxArea(grid, row + d_row, col + d_col,max_val)
+    #     return max_val
+    def maxArea(self, grid, row, col):
+        rows = len(grid)
+        cols = len(grid[0])
+        if row < 0 or row >= rows or col < 0 or col >= cols:
+            return 0
+        if grid[row][col] == 0:
+            return 0
+        grid[row][col] = 0
+        area = 1
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        # 从这里开始递归
+        for d_row, d_col in directions:
+            area += self.maxArea(grid, row + d_row, col + d_col)
+        return area
